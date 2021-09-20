3e410a1299c7fa1f7edda6918afe2bc3c5f49394
Ohata Kenji <swordgram@gmail.com>
2021-09-21 03:53:20 +0900

    TST: add three paragraph test

--- test/stlap.test.ts
+++ test/stlap.test.ts
@@ -1,7 +1,7 @@
 import { parse, Stlap } from "../src/stlap";
 
 describe("parse", () => {
-  test("small", () => {
+  test("One paragraph", () => {
     const input =
       "//comment\nHello World\nthis is first test of stlap\n「Have a good day」";
     const output = parse(input);
@@ -11,6 +11,32 @@ describe("parse", () => {
         source: input,
         text: "Hello Worldthis is first test of stlap「Have a good day」",
         lines: "Have a good day",
+      },
+    ];
+
+    expect(output).toEqual<Stlap | Error>(expected);
+  });
+
+  test("Three paragraph", () => {
+    const input =
+      "//comment\nHello World\nthis is first test of stlap\n\n\n\n「Have a good day」\n\nTESTTESTTEST\n//comment2";
+    const output = parse(input);
+
+    const expected = [
+      {
+        source: "//comment\nHello World\nthis is first test of stlap",
+        text: "Hello Worldthis is first test of stlap",
+        lines: "",
+      },
+      {
+        source: "「Have a good day」",
+        text: "「Have a good day」",
+        lines: "Have a good day",
+      },
+      {
+        source: "TESTTESTTEST\n//comment2",
+        text: "TESTTESTTEST",
+        lines: "",
       },
     ];
 
