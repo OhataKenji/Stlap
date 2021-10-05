a8479e940d493c13c02cfcf9fbc964366a11f531
Ohata Kenji <11045367+OhataKenji@users.noreply.github.com>
2021-10-05 23:05:37 +0900

    BLD: faster toText

--- src/stlap.ts
+++ src/stlap.ts
@@ -47,69 +47,69 @@ export class Stlap {
   }
 
   toText(): string {
-    return this._toText(this.story);
+    return this._toTextArray(this.story).join("");
   }
 
-  _toText(v: Vertex | Token, paragraphSeparater = "\n\n"): string {
+  _toTextArray(v: Vertex | Token, paragraphSeparater = "\n\n"): string[] {
     if (v instanceof Token) {
       if (v.kind === TokenKind.Words) {
         const r = v.getTextRange();
         if (r === null) {
-          return ""; // TODO better error handling
+          return [""]; // TODO better error handling
         }
         const s = this.numberOfCharUntilLine[r.start.line] + r.start.charcter;
         const e = this.numberOfCharUntilLine[r.end.line] + r.end.charcter;
-        return this.source.slice(s, e + 1);
+        return [this.source.slice(s, e + 1)];
       } else {
-        return "";
+        return [""];
       }
     } else {
       if (v.kind === Vertexkind.Sentence) {
         return v.children
-          .map((c) => this._toText(c, paragraphSeparater))
-          .join("");
+          .map((c) => this._toTextArray(c, paragraphSeparater))
+          .flat();
       } else if (v.kind === Vertexkind.Paragraph) {
         return v.children
-          .map((c) => this._toText(c, paragraphSeparater))
-          .join("");
+          .map((c) => this._toTextArray(c, paragraphSeparater))
+          .flat();
       } else if (v.kind === Vertexkind.ParagraphSeparater) {
-        return paragraphSeparater;
+        return [paragraphSeparater];
       } else if (v.kind === Vertexkind.Story) {
         const textAndSeps = v.children.map((c) =>
           c.kind === Vertexkind.ParagraphSeparater
             ? c
-            : this._toText(c, paragraphSeparater)
+            : this._toTextArray(c, paragraphSeparater)
         );
 
         // ""を挟むようなparagraphSepapraterの片方を消去
-        // [..., PS, "", PS, ...] -> [..., "", "", PS, ...]
+        // [..., PS,[""], PS, ...] -> [...,[""], [""], PS, ...]
         for (let i = 0; i < textAndSeps.length - 2; i++) {
           if (
             textAndSeps[i] instanceof Vertex &&
-            textAndSeps[i + 1] === "" &&
+            textAndSeps[i + 1].toString() === [""].toString() && // TODO better array comparison
             textAndSeps[i + 2] instanceof Vertex
           ) {
-            textAndSeps[i] = "";
+            textAndSeps[i] = [""];
           }
         }
 
         // 先頭は""，末尾は"\n"
         if (textAndSeps[0] instanceof Vertex) {
-          textAndSeps[0] = "";
+          textAndSeps[0] = [""];
         }
         if (textAndSeps[textAndSeps.length - 1] instanceof Vertex) {
-          textAndSeps[textAndSeps.length - 1] = "\n";
+          textAndSeps[textAndSeps.length - 1] = ["\n"];
         } else {
-          textAndSeps.push("\n");
+          textAndSeps.push(["\n"]);
         }
 
         return textAndSeps
           .map((v) =>
-            v instanceof Vertex ? this._toText(v, paragraphSeparater) : v
+            v instanceof Vertex ? this._toTextArray(v, paragraphSeparater) : v
           )
-          .join("");
+          .flat();
       } else {
-        return "";
+        return [""];
       }
     }
   }
